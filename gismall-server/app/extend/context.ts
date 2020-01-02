167460337e34357727b682409013405ffee7f674
xiaowei.xue <xiaowei.xue@shopee.com>
2020-01-02 18:02:11 +0800

    chore: remove useless files

--- gismall-server/app/extend/context.ts
+++
@@ -1,9 +0,0 @@
-import { Context } from 'egg';
-export default {
-  get pathname(this: Context) {
-    return '';
-  },
-  getPathname(this: Context) {
-    return this.path.substr(0, this.path.indexOf('?'));
-  },
-};
